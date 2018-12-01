a6fc04aebcfc02856b07e6ca1668d636e0c80219
ilyshander <mr.iluxin@yandex.ru>
2018-12-01 23:28:56 +0300

    Сделал программу
    Проверьте пожалуйста

---
+++ base/CPU.h
@@ -0,0 +1,34 @@
+﻿#ifndef __CPU_H__
+#define __CPU_H__
+
+#include "queue.h"
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+struct Task
+{
+	int cors;
+};
+
+struct Statistics
+{
+	int taskSl;
+	int taskN;
+	int taskR;
+	double taskRCPU() { return ((double)taskR / (double)taskN); }
+	double taskSlCPU() { return ((double)taskSl / (double)taskN); }
+};
+
+
+class TCPU
+{
+	TQueue<Task> queue;
+	int corsN;
+public:
+	TCPU(int queueSize, int coreNums) : queue(queueSize), corsN(coreNums) {}
+	Statistics Run(double q1, double q2, int time);
+};
+
+#endif
---
+++ base/sample.cpp
@@ -0,0 +1,36 @@
+﻿#include "CPU.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string time;
+	string queue = "";
+	string q1 = "";
+	string q2 = "";
+	string cores = "";
+
+	cout << "Tacts count: ";
+	cin >> time;
+	cout << "Queue size: ";
+	cin >> queue;
+	cout << "Task intensity: ";
+	cin >> q1;
+	cout << "Proc power: ";
+	cin >> q2;
+	cout << "Cores count: ";
+	cin >> cores;
+
+	TCPU p(stoi(queue), stoi(cores));
+	auto s = p.Run(stod(q1.c_str()), stod(q2.c_str()), stoi(time));
+
+	cout << "\n";
+	cout << "Task count " << s.taskN << endl;
+	cout << "Task rejected " << s.taskR << " or " << s.taskRCPU() << "% " << endl;
+	cout << "Sleep time " << s.taskSl << " or " << s.taskSlCPU() << "% " << endl;
+
+	system("pause");
+
+	return 0;
+}
